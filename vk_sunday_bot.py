2688377c6e2131d17affddd33384d1f13dd54d90
spivak1vladimir <connectspivak@gmail.com>
2026-03-21 01:51:45 +0300

    Update vk_sunday_bot.py

--- vk_sunday_bot.py
+++ vk_sunday_bot.py
@@ -4,8 +4,7 @@ import logging
 import asyncio
 from datetime import datetime, timedelta
 
-from vkbottle.bot import Bot, Message, Blueprint
-from vkbottle.keyboard import Keyboard, Text, KeyboardButtonColor
+from vkbottle.bot import Bot, Message, Blueprint, Keyboard, KeyboardButton, KeyboardButtonColor
 
 # ---------------- НАСТРОЙКИ ----------------
 VK_TOKEN = "vk1.a.VNTxYTHvQMbbRQFFZyY7575TCJrJSYPN4CxIBc9u-PdamXSD0-iy2BDOBtkviwfC-BNtnE1qwEraCM-USWlrvf6arvuGcSgd2qeY9KaUCecbJyQklhgiKhvJYz8b8q9GxBei_52VN4UDjsKGLGWI1w7h7Ensf7MzeonRguZfGdY41Oc6tBx-nJSB8IKRv4xYvlyLf39ieMJl1iF0zjWXdA"
@@ -47,10 +46,10 @@ def build_info_text():
 
 def main_keyboard():
     keyboard = Keyboard(one_time=False)
-    keyboard.add(Text("Регистрация", color=KeyboardButtonColor.POSITIVE))
-    keyboard.add(Text("Отменить регистрацию", color=KeyboardButtonColor.NEGATIVE))
+    keyboard.add(KeyboardButton("Регистрация", color=KeyboardButtonColor.POSITIVE))
+    keyboard.add(KeyboardButton("Отменить регистрацию", color=KeyboardButtonColor.NEGATIVE))
     keyboard.row()
-    keyboard.add(Text("Информация о забеге", color=KeyboardButtonColor.PRIMARY))
+    keyboard.add(KeyboardButton("Информация о забеге", color=KeyboardButtonColor.PRIMARY))
     return keyboard.get_json()
 
 # ---------------- БОТ ----------------
